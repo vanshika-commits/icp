4ec8111dd9d8e1fd3bea6469f65a0ce362f65c4f
vanshika-commits <vanshikagoyal2107@gmail.com>
2025-09-29 18:11:47 +0530

    Create assignment1.java

---
+++ assignment1.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0) {
+            return nums;
+        }
+        int[] result = new int[n - k + 1];
+        LinkedList<Integer> dq = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
+                dq.poll();
+            }
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offer(i);
+            if (i - k + 1 >= 0) {
+                result[i - k + 1] = nums[dq.peek()];
+            }
+        }
+        return result;
+    }
+}
+//2
+lass Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+    int first = -1, last = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (first == -1) first = i;
+                last = i;                  
+            }
+        }
+
+        return new int[]{first, last};
+    }
+}
