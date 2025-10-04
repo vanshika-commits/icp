2f771d86bb4371f898755b8a8ee9518dac95c43e
vanshika-commits <vanshikagoyal2107@gmail.com>
2025-10-04 21:24:52 +0530

    Create assign 7.java

---
+++ assign 7.java
@@ -0,0 +1,44 @@
+//1 unique path
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+//2 house roober
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        int prev1 = 0, prev2 = 0;
+        for(int num : nums) {
+            int temp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = temp;
+        }
+        return prev1;
+    }
+}
+//3 maximum subarray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
